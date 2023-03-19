8388940506c994ea93936bf917476e1e42391e7f
Adnan <mirzaabbas171@gmail.com>
2023-03-19 19:11:49 +0500

    make pages responsive

--- pages/itinerary/[itineraryId].js
+++ pages/itinerary/[itineraryId].js
@@ -1,27 +1,32 @@
-import { getSession, withPageAuthRequired } from '@auth0/nextjs-auth0';
-import { faHashtag } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useRouter } from 'next/router';
-import { useContext, useRef, useState } from 'react';
-import { AppLayout } from '../../components/AppLayout';
-import ItinerariesContext from '../../context/itinerariesContext';
-import { getAppProps } from '../../utils/getAppProps';
+import { getSession, withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { faHashtag } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRouter } from "next/router";
+import { useContext, useRef, useState } from "react";
+import { AppLayout } from "../../components/AppLayout";
+import ItinerariesContext from "../../context/itinerariesContext";
+import { getAppProps } from "../../utils/getAppProps";
 import Linkify from "react-linkify";
-import jsPDF from 'jspdf';
-import validator from 'validator'
-import { doc, getDoc } from 'firebase/firestore';
-import { db } from '../../firebase.config';
-import { useIntl } from 'react-intl';
+import jsPDF from "jspdf";
+import validator from "validator";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../../firebase.config";
+import { useIntl } from "react-intl";
 
 const componentDecorator = (href, text, key) => (
-  <a className="linkify__text" style={{ fontSize: "8px", fontWeight: "bold", color: "blue" }} href={href} key={key} target="_blank" rel='noreferrer'>
+  <a
+    className="linkify__text"
+    style={{ fontSize: "8px", fontWeight: "bold", color: "blue" }}
+    href={href}
+    key={key}
+    target="_blank"
+    rel="noreferrer"
+  >
     {text}
   </a>
 );
 
-
 export default function Itinerary(props) {
-
   const intl = useIntl();
 
   const getText = (id) => intl.formatMessage({ id });
@@ -35,9 +40,9 @@ export default function Itinerary(props) {
   const handleDeleteConfirm = async () => {
     try {
       const response = await fetch(`/api/deleteItinerary`, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'content-type': 'application/json',
+          "content-type": "application/json",
         },
         body: JSON.stringify({ itineraryId: props.id }),
       });
@@ -46,7 +51,7 @@ export default function Itinerary(props) {
         deleteItinerary(props.id);
         router.replace(`/itinerary/new`);
       }
-    } catch (e) { }
+    } catch (e) {}
   };
 
   const divRef = useRef(null);
@@ -57,7 +62,7 @@ export default function Itinerary(props) {
     var email = e.target.value;
 
     if (validator.isEmail(email)) {
-      setEmailError(getText('itineraryid.emailok'));
+      setEmailError(getText("itineraryid.emailok"));
       setEmailOk(e.target.value);
     } else {
       setEmailError("itineraryid.emailerror");
@@ -67,11 +72,13 @@ export default function Itinerary(props) {
   const emailContent = props.title + props.itineraryContent;
 
   const pdfDownload = (e) => {
-    e.preventDefault()
-    let doc = new jsPDF("landscape", 'pt', 'A4');
+    e.preventDefault();
+    let doc = new jsPDF("landscape", "pt", "A4");
     const content = document.getElementById("pdf-view");
 
     content.style.fontSize = "12px";
+    content.style.width = "1050px";
+    content.style.lineHeight = "24px";
 
     const options = {
       callback: () => {
@@ -79,12 +86,12 @@ export default function Itinerary(props) {
       },
       x: 5,
       y: 5,
-      html2canvas: { scale: 0.8 }
+      html2canvas: { scale: 0.8 },
     };
 
     // Generar el PDF
     doc.html(content, options);
-    console.log(content)
+    console.log(content);
   };
 
   return (
@@ -93,7 +100,7 @@ export default function Itinerary(props) {
         {props.apiOutput && (
           <div className="output">
             <div className="output-content">
-              <p>
+              <p style={{ wordBreak: "break-all" }}>
                 <Linkify componentDecorator={componentDecorator}>
                   {props.apiOutput}
                 </Linkify>
@@ -103,70 +110,78 @@ export default function Itinerary(props) {
                   <div dangerouslySetInnerHTML={{ __html: props.info }}></div>
                 </div>
               )}
-              <span style={{ fontWeight: "bold", color: "black" }}>
-                {getText('itineraryid.sendemail')}
-              </span>
-              <input
-                type="text"
-                style={{ fontWeight: "bold", color: "black", borderColor: 'lightblue', borderStyle: 'solid', borderWidth: '2px' }}
-                id="userEmail"
-                onChange={(e2) => validateEmail(e2)}
-              ></input>
-              <br />
-              <span style={{ fontWeight: "bold", color: "red" }}>
-                {emailError}
-              </span>
-
-              <br />
-
-              <div class="front mb-2">
-                <span>
-                  <a
-                    href={`mailto:${emailOk || "edu@edu.com"
-                      }?subject=${encodeURIComponent(
-                        subject
-                      )}&body=${encodeURIComponent(props.apiOutput)}`}
-                  >
-                    {getText('itineraryid.sendplan')}
-                  </a>
-                </span>
-              </div>
-              <div class="front">
-                <button onClick={pdfDownload}>&nbsp;&nbsp;&nbsp;&nbsp;{getText('itineraryid.genpdf')}</button>
-              </div>
-
-
             </div>
           </div>
         )}
       </div>
+      {props.apiOutput && (
+        <div className="w-full">
+          <span style={{ fontWeight: "bold", color: "black" }}>
+            {getText("itineraryid.sendemail")}
+          </span>
+          <input
+            type="text"
+            style={{
+              fontWeight: "bold",
+              color: "black",
+              borderColor: "lightblue",
+              borderStyle: "solid",
+              borderWidth: "2px",
+            }}
+            id="userEmail"
+            onChange={(e2) => validateEmail(e2)}
+          ></input>
+          <br />
+          <span style={{ fontWeight: "bold", color: "red" }}>{emailError}</span>
 
+          <br />
+
+          <div class="front mb-2">
+            <span>
+              <a
+                href={`mailto:${
+                  emailOk || "edu@edu.com"
+                }?subject=${encodeURIComponent(
+                  subject
+                )}&body=${encodeURIComponent(props.apiOutput)}`}
+              >
+                {getText("itineraryid.sendplan")}
+              </a>
+            </span>
+          </div>
+          <div class="front">
+            <button onClick={pdfDownload}>
+              &nbsp;&nbsp;&nbsp;&nbsp;{getText("itineraryid.genpdf")}
+            </button>
+          </div>
+        </div>
+      )}
       <div className="my-4">
         {!showDeleteConfirm && (
           <button
             className="btn bg-red-600 hover:bg-red-700"
             onClick={() => setShowDeleteConfirm(true)}
           >
-            {getText('itinerayid.delete')}
+            {getText("itinerayid.delete")}
           </button>
         )}
         {!!showDeleteConfirm && (
           <div>
             <p className="p-2 bg-red-300 text-center">
-              {getText('itineraryid.deleteconfirm')}
+              {getText("itineraryid.deleteconfirm")}
             </p>
             <div className="grid grid-cols-2 gap-2">
               <button
                 onClick={() => setShowDeleteConfirm(false)}
                 className="btn bg-stone-600 hover:bg-stone-700"
               >
-                {getText('itineraryid.canceldel')}
+                {getText("itineraryid.canceldel")}
               </button>
               <button
                 onClick={handleDeleteConfirm}
                 className="btn bg-red-600 hover:bg-red-700"
               >
-                {getText('itineraryid.confirmdel')}
+                {getText("itineraryid.confirmdel")}
               </button>
             </div>
           </div>
@@ -186,18 +201,22 @@ export const getServerSideProps = withPageAuthRequired({
     const userSession = await getSession(ctx.req, ctx.res);
 
     // get user data from firestore
-    const docRef = doc(db, "users", `${userSession.user.sub}-${userSession.user.email}`);
+    const docRef = doc(
+      db,
+      "users",
+      `${userSession.user.sub}-${userSession.user.email}`
+    );
     const userDocSnap = await getDoc(docRef);
 
     // get itinerary data from firestore
     const itinerariesDocRef = doc(db, "itineraries", ctx.params.itineraryId);
     const itineraryDocSnap = await getDoc(itinerariesDocRef);
 
-// if itinerary doesn't exist, redirect to new itinerary page
+    // if itinerary doesn't exist, redirect to new itinerary page
     if (!itineraryDocSnap.exists()) {
       return {
         redirect: {
-          destination: '/itinerary/new',
+          destination: "/itinerary/new",
           permanent: false,
         },
       };
